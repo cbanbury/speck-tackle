65efa56591790e01f8bc21baac72bb353f9082c6
Carl Banbury <carl@googolmail.co.uk>
2016-03-15 19:50:53 +0000

    first release to atmosphere

--- package.js
+++ package.js
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use(['ecmascript', 'jquery']);
-  api.use('d3js:d3');
+  api.use('d3js:d3@3.5.8');
   api.addFiles('st.min.js');
 });
 
